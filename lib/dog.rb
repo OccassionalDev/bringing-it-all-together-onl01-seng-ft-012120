84255541925b19ee4c06eee4bd23eb746d074344
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-21 05:07:10 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -52,4 +52,8 @@ class Dog
     DB[:conn].execute(sql, self.name, self.breed)
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
   end 
+  
+  def update 
+    
+  end 
 end 
